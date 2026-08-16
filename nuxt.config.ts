4e8db218b3006f00393bac3a4971d6f0ceddd7b6
nikzz-tea <84617815+nikzz-tea@users.noreply.github.com>
2026-08-16 23:42:02 +0300

    chore: enable @vercel/speed-insights module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
     osuClientSecret: process.env.OSU_CLIENT_SECRET || '',
   },
   css: ['~/assets/css/tailwind.css'],
-  modules: ['shadcn-nuxt', '@vercel/analytics'],
+  modules: ['shadcn-nuxt', '@vercel/analytics', '@vercel/speed-insights'],
   shadcn: {
     prefix: '',
   },
