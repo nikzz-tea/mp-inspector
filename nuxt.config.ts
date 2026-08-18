f3f42e13a50cd57376583ee827a061841f1cef8a
nikzz-tea <84617815+nikzz-tea@users.noreply.github.com>
2026-08-19 02:57:25 +0300

    chore: install @nuxtjs/robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
     osuClientSecret: process.env.OSU_CLIENT_SECRET || '',
   },
   css: ['~/assets/css/tailwind.css'],
-  modules: ['shadcn-nuxt', '@vercel/analytics', '@vercel/speed-insights'],
+  modules: ['shadcn-nuxt', '@nuxtjs/robots', '@vercel/analytics', '@vercel/speed-insights'],
   shadcn: {
     prefix: '',
   },
